9c313851bb6c61db04986ee29851846f71719338
MobinAskariN <160580813+MobinAskariN@users.noreply.github.com>
2024-07-27 19:26:17 +0330

    reading data from "station" table

---
+++ GraphWithLabels/Controllers/StationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GraphWithLabels.Models;
+using System.Linq;
+
+namespace GraphWithLabels.Controllers
+{
+    public class StationController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public StationController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var stationId = 1;
+            var station = _context.station
+                                  .FirstOrDefault(s => s.stationId == stationId);
+            if (station != null)
+            {
+                ViewBag.StationName = station.stataionName;
+            }
+            else
+            {
+                ViewBag.StationName = "Station not found";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ GraphWithLabels/Models/Station.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GraphWithLabels.Models
+{
+    public class Station
+    {
+        public int stationId { get; set; }  // Maps to the primary key in your table
+        public string stataionName { get; set; }  // Maps to the stationName column
+    }
+
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Station> station { get; set; }  // Represents the Stations table
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+
+}
