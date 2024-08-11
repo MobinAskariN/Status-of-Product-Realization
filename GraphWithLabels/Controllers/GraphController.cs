98abc2327f671b4c6e35801f276b93388686bc46
MobinAskariN <160580813+MobinAskariN@users.noreply.github.com>
2024-08-11 20:47:10 +0330

    doc information improvement

--- GraphWithLabels/Controllers/GraphController.cs
+++ GraphWithLabels/Controllers/GraphController.cs
@@ -221,7 +221,7 @@ namespace GraphWithLabels.Controllers
                 Console.WriteLine (e.Item1.id + " " + e.Item2.id);
 
 
-            ViewBag.SvgWidth = labels.Count * 150 + 100;
+            ViewBag.SvgWidth = labels.Count * 150;
             ViewBag.Svgheight = max_nv * 120 + 300;
             ViewBag.Labels = labels;
             ViewBag.Edges = edges;
@@ -229,43 +229,3 @@ namespace GraphWithLabels.Controllers
         }
     }
 }
-
-
-
-
-
-/*
-// Example from the image
-var labels = new List<Label>
-{
-    new Label("1", 1),
-    new Label("2", 1),
-    new Label("3", 4),
-    new Label("4", 4),
-    new Label("5", 1)
-};
-
-// Add edges
-var edges = new List<(Vertex, Vertex)>
-{
-    (labels[0].getVertices()[0], labels[1].getVertices()[0]),
-
-    (labels[1].getVertices()[0], labels[2].getVertices()[0]),
-    (labels[1].getVertices()[0], labels[2].getVertices()[1]),
-    (labels[1].getVertices()[0], labels[2].getVertices()[2]),
-    (labels[1].getVertices()[0], labels[2].getVertices()[3]),
-
-    (labels[2].getVertices()[0], labels[3].getVertices()[0]),
-    (labels[2].getVertices()[1], labels[3].getVertices()[1]),
-    (labels[2].getVertices()[2], labels[3].getVertices()[2]),
-    (labels[2].getVertices()[3], labels[3].getVertices()[3]),
-
-    (labels[3].getVertices()[0], labels[4].getVertices()[0]),
-    (labels[3].getVertices()[1], labels[4].getVertices()[0]),
-    (labels[3].getVertices()[2], labels[4].getVertices()[0]),
-    (labels[3].getVertices()[3], labels[4].getVertices()[0])
-};
-
-ViewBag.Labels = labels;
-ViewBag.Edges = edges;
-*/
--- GraphWithLabels/obj/Debug/net8.0/GraphWithLabels.AssemblyInfo.cs
+++ GraphWithLabels/obj/Debug/net8.0/GraphWithLabels.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraphWithLabels")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2c02fd1ef08b977580eaa7eaff4ab55f111aa9d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7967365c50c69a79ab8b183f6aba2f942b6bd862")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraphWithLabels")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraphWithLabels")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
